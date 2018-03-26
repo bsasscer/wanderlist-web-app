47cebe9377c9cf05a546af8dd812d0457ef15eb2
bsasscer <smithbi.maricela@gmail.com>
2018-03-26 15:01:04 -0700

    add tentative project structure

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            photos: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://api.unsplash.com/photos/?client_id=YOUR-API-KEY')
+        .then(response => response.json())
+        .then(responseData => {
+            this.setState({ photos: responseData });
+        })
+        .catch(error => {
+            console.log('Error fetching and parsing data', error);
+        });
+    }
+
+    render() {
+        console.log(this.state.photos);
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Hello World</h1>
+            </header>
+            <p className="App-intro">
+
+            </p>
+          </div>
+        );
+    }
 }
 
 export default App;
