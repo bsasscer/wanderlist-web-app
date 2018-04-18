2b19402e40ed92dcee722168b788a30d5d0103ec
bsasscer <smithbi.maricela@gmail.com>
2018-04-18 10:57:22 -0700

    ui tweaks for post tiles

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -8,13 +8,24 @@ class Photo extends Component {
 
         return (
             <div className="tile">
-                <div className="post-img">
-                    <img src={photo.urls.small} alt={photo.id} />
+                <div className="post-img" style={{ backgroundImage: `url(${photo.urls.regular})`}}>
                 </div>
+                <span className="user-info">
+                    <div className="user-img">
+                        <a href={photo.user.links.html}>
+                            <img src={photo.user.profile_image.medium} className="user-profile-img" alt={photo.user.name} />
+                        </a>
+                    </div>
+                    <div>
+                        <a href={photo.user.links.html}>
+                            <span className="user-name">{photo.user.name}</span>
+                        </a>
+                    </div>
+                </span>
                 <div className="post-info">
-                    <img src={photo.user.profile_image.small} className="user-profile-img" alt={photo.user.name} />
-                    <span className="user-name">{photo.user.name}</span>
-                    <p>Likes: {photo.likes}</p>
+                    <span>Likes: {photo.likes}</span>
+                    <span>Location: </span>
+                    <span>Save</span>
                 </div>
             </div>
         );
