137c42f47f7d6c48c56f0987c93b67ee0d23f843
bsasscer <smithbi.maricela@gmail.com>
2018-05-09 10:44:01 -0700

    add Profile model

---
+++ backend/models/Profile.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  loves: {
+    type: [String]
+  },
+  bio: {
+    type: String
+  },
+  social: {
+    twitter: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
