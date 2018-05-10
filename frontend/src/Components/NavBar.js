515b2019db88405d93082fdf708a96d6019aa920
bsasscer <smithbi.maricela@gmail.com>
2018-05-10 15:40:16 -0700

    tweak navbar and profile contents

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function NavBar () {
-    
-    return (
-        <nav className="nav-outer">
-            <ul className="nav-inner">
-                <li><NavLink exact to="/">Home</NavLink></li>
-                <li><NavLink to="/discover">Discover</NavLink></li>
-                <li><NavLink to="/trips">Trips</NavLink></li>
-                <li><NavLink to="/wishlist">Wishlist</NavLink></li>
-                <li><NavLink to="/profile">Profile</NavLink></li>
-            </ul>
-        </nav>
-    )
+function NavBar() {
+  return (
+    <nav className="nav-outer">
+      <ul className="nav-inner">
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/discover">Discover</NavLink>
+        </li>
+        <li>
+          <NavLink to="/wishlist">Wishlist</NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import Trips from './Trips';
 
 class Profile extends Component {
-
-    render() {
-
-        return (
-            <div>
-                <p>This is where the user profile will go!</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>This is where the user profile will go!</p>
+        <Trips />
+      </div>
+    );
+  }
 }
-
 
 export default Profile;
