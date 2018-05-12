6e80cf9440dfc6c9a91cd8625aba8bdc61b6de9d
bsasscer <smithbi.maricela@gmail.com>
2018-05-11 17:12:45 -0700

    change header image for better optimization

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  state = {
+    title: 'Wanderlist',
+    subtitle: 'Your Visual Travel Planner'
+  };
 
-    constructor() {
-        super();
-        this.state = {
-            title: 'Wanderlist',
-            subtitle: 'Your Visual Travel Planner'
-        };
-    }
-
-    render() {
-
-        return (
-            <header>
-                <div className="overlay"></div>
-                <div className="splash">
-                    <h1>{this.state.title}</h1>
-                    <h2>{this.state.subtitle}</h2>
-                </div>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <div className="overlay">
+          <h1>{this.state.title}</h1>
+          <h2>{this.state.subtitle}</h2>
+        </div>
+        <div className="splash" />
+        <a
+          className="credit-badge"
+          href="https://www.twenty20.com/canipel/photos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>@canipel via Twenty20</span>
+        </a>
+      </header>
+    );
+  }
 }
 
 export default Header;
